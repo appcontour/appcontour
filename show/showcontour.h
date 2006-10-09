3edd4aa232d24187618ac5204c38c51e145faf9a
Maurizio Paolini <maurizio.paolini@unicatt.it>
2006-10-09 07:45:47 +0000

    forgot to add this file

---
+++ show/showcontour.h
@@ -0,0 +1,39 @@
+struct morseevent {
+  int type;
+  int ori;
+  int ori2;
+  int cusps;
+  int cusps2;
+  struct morseevent *next;
+};
+
+struct polyline {
+  struct vertex *vertex;
+  struct line *line;
+  int numvertices;
+  double h;
+};
+
+struct vertex {
+  int tag;
+  int type;
+  double x;
+  double y;
+  struct vertex *next;
+  struct line *line[];
+};
+
+struct line {
+  int tag;
+  int orientation;
+  int cusps;
+  struct vertex *a;
+  struct vertex *b;
+  struct line *next;
+};
+
+/* prototypes */
+
+int grmain (int *argcpt, char *argv[]);
+void evolve (struct polyline *contour, double incrtime);
+
