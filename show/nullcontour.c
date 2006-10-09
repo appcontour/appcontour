24311d74d16b7795417f5f44f494f76b8e1e2456
Maurizio Paolini <maurizio.paolini@unicatt.it>
2006-10-09 07:47:18 +0000

    forgot this also.

---
+++ show/nullcontour.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "showcontour.h"
+
+extern struct polyline *contour;
+extern double incrtime;
+
+void idle (void);
+
+void
+display (void)
+{
+}
+
+void
+menu (int value)
+{
+}
+
+void
+visible (int state)
+{
+}
+
+void
+idle (void)
+{
+}
+
+int
+grmain (int *argcpt, char *argv[])
+{
+  return (0);
+}
+
