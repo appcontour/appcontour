f8bff9297b9e66447172f742ce027d27c3f5207d
Maurizio Paolini <maurizio.paolini@unicatt.it>
2010-09-28 14:37:13 +0000

    contour_describe in zork style; new command ccchilds <ccid> for contour

--- utils/contour_describe.sh
+++ utils/contour_describe.sh
@@ -4,7 +4,7 @@
 
 function usage ()
 {
-  echo "usage: $0 [-v] <contour-description-file>"
+  echo "usage: $0 [-z] <contour-description-file>"
 }
 
 function mynumber ()
@@ -63,27 +63,6 @@ function describe ()
     echo -n "$separator"
     plural="1"
     if [ "$num" = "1" ]; then plural=""; fi
-#    case $num in
-#      0)
-#        thenum="no"
-#      ;;
-#      1)
-#        thenum="one"
-#      ;;
-#      2)
-#        thenum="two"
-#      ;;
-#      3)
-#        thenum="three"
-#      ;;
-#      *)
-#        thenum=$num
-#        if [ -n "$nnumber" ]
-#        then
-#          thenum=`$nnumber -l $num`
-#        fi
-#      ;;
-#    esac
     thenum=`mynumber $num`
 
     case $holes in
@@ -121,14 +100,86 @@ function describe ()
   done
 }
 
-declare -a holes
-verbose=""
+function zorkdescribe ()
+{
+  ccid=$1
+  count=0
 
-while getopts "v" option
+  list=`$ccontour -q ccchilds $ccid $file 2>/dev/null`
+  if [ -n "$list" ]
+  then
+    if [ "$ccid" = "0" ]
+    then
+      echo -n "You can see"
+    else
+      echo -n "The ${description[$ccid]} contains"
+    fi
+    objnum=`echo $list | wc -w`
+    for o in $list
+    do
+      count=$[ $count + 1 ]
+      if [ "$count" -gt "1" ]
+      then
+        if [ "$count" -lt "$objnum" ]
+        then
+          echo -n ","
+        else
+          echo -n " and"
+        fi
+      fi
+      echo -n " a ${description[$o]}"
+    done
+    echo "."
+  else
+    if [ -n "$list" ]
+    then
+      echo "You can see nothing."
+#    else
+#      echo "The ${description[$ccid]} contains nothing."
+    fi
+  fi
+  for o in $list
+  do
+    zorkdescribe $o
+  done
+}
+
+declare -a holes
+declare -a description
+declare -a objwithgivenholes
+declare -a digits
+declare -a counter
+declare -a adjective
+
+adjective[0]="white"
+adjective[1]="black"
+adjective[2]="red"
+adjective[3]="blue"
+adjective[4]="green"
+adjective[5]="purple"
+adjective[6]="brown"
+adjective[7]="yellow"
+adjective[8]="pink"
+adjective[9]="violet"
+
+material[0]="metal"
+material[1]="stone"
+material[2]="marble"
+material[3]="wooden"
+material[4]="granite"
+material[5]="gold"
+material[6]="silver"
+material[7]="bronze"
+material[8]="plastic"
+material[9]="sandstone"
+
+zork=""
+
+while getopts "z" option
 do
   case $option in
-  v)
-    verbose=1
+  z)
+    zork=1
     ;;
   *)
     echo "Invalid option"
@@ -167,7 +218,12 @@ fi
 
 if ! $ccontour ishuffman $file -q 2>/dev/null
 then
-  echo "This is not a contour with Huffman labelling"
+  if [ -n "$zork" ]
+  then
+    echo "It is pitch black, you are likely to be eaten by a grue."
+  else
+    echo "This is not a contour with Huffman labelling"
+  fi
   exit 2
 fi
 
@@ -177,7 +233,14 @@ lista=""
 for comp in `seq $cc`
 do
   euler=`$ccontour extractcc $comp $file 2>/dev/null | $ccontour characteristic -q 2>/dev/null`
-  holes[$comp]=$[ ( 2 - $euler ) / 2 ]
+  nholes=$[ ( 2 - $euler ) / 2 ]
+  holes[$comp]=$nholes
+  count=${objwithgivenholes[$nholes]}
+  digits[$nholes]=${#count}
+  counter[$nholes]="0"
+  if [ -z "$count" ]; then count="0"; fi
+  count=$[ $count + 1 ]
+  objwithgivenholes[$nholes]=$count
   orientation=`$ccontour -q ccorientation $comp $file 2>/dev/null`
   # echo "component $comp, ${holes[$comp]} holes"
   if [ -z "$lista" ]
@@ -188,18 +251,92 @@ do
   fi
 done
 
+for comp in `seq $cc`
+do
+  nholes=${holes[$comp]}
+  ldigits=${digits[$nholes]}
+  count=${counter[$nholes]}
+  lcount=$count
+  adj=""
+  if [ "$ldigits" -gt 0 ]
+  then
+    idx=$[ $lcount % 10 ]
+    lcount=$[ $lcount / 10 ]
+    adj=${adjective[$idx]}
+    ldigits=$[ $ldigits - 1 ]
+  fi
+  if [ "$ldigits" -gt 0 ]
+  then
+    idx=$[ $lcount % 10 ]
+    lcount=$[ $lcount / 10 ]
+    adj="$adj ${material[$idx]}"
+    ldigits=$[ $ldigits - 1 ]
+  fi
+  for iter in `seq $ldigits`
+  do
+    idx=$[ $lcount % 10 ]
+    lcount=$[ $lcount / 10 ]
+    adj="${adjective[$idx]} $adj"
+    ldigits=$[ $ldigits - 1 ]
+  done
+  case $nholes in
+    0)
+      descr="sphere"
+      ;;
+    1)
+      descr="torus"
+      ;;
+    *)
+      theholes=`mynumber $nholes`
+      descr="sphere with $theholes handles"
+      ;;
+  esac
+  if [ -n "$adj" ]
+  then
+    descr="$adj $descr"
+  fi
+  description[$comp]="$descr"
+  count=$[ $count + 1 ]
+  counter[$nholes]=$count
+done
+
 lista=`echo "$lista" | tr '#' '\n' | sort -n`
 listanew=`echo "$lista" | cut -f1 -d':' | uniq -c | tr -s ' '`
 totoutput=`echo "$listanew" | wc -l`
 totoutput=`echo $totoutput`
 
-if [ "$cc" -gt "1" ]
+if [ -n "$zork" ]
 then
-  echo -n "There are "
-else
-  echo -n "There is "
+  echo "Clearing"
+  echo "You are in a clearing, with a forest surrounding you on all sides."
+  case $cc in
+    0)
+      echo "There are no objects here."
+      ;;
+    1)
+      echo "There is one object here."
+      echo ""
+      ;;
+    *)
+      thecc=`mynumber $cc`
+      echo "There is a total of $thecc objects here."
+      echo ""
+      ;;
+  esac
 fi
 
-echo "$listanew" | describe
+if [ -n "$zork" ]
+then
+  zorkdescribe 0
+else
+  if [ "$cc" -gt "1" ]
+  then
+    echo -n "There are "
+  else
+    echo -n "There is "
+  fi
 
-echo "."
+  echo "$listanew" | describe
+
+  echo "."
+fi
