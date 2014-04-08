5107844ecb98eb41caaeda2189f074d70daf20e7
Maurizio Paolini <maurizio.paolini@unicatt.it>
2014-04-08 12:49:46 +0000

    partial additions

--- alexander.h
+++ alexander.h
@@ -2,6 +2,15 @@
  * definitions for alexander polynomial
  */
 
+struct laurentpoly {
+  int minexpon;
+  int stemdegree;
+  int denom;
+  int stem[];
+};
+
 /* prototypes */
 
 int alexander (struct presentation *p);
+struct laurentpoly laurent_get_exp_sum (struct presentationrule *r, int g, int gconj);
+void print_laurentpoly (struct laurentpoly *l);
