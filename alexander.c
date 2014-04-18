d97ab28672c4cf3c58af507e2c78aef9325434b0
Maurizio Paolini <maurizio.paolini@unicatt.it>
2014-04-18 17:25:13 +0000

    basic implementation of alexander polynomial in two indeterminates

--- alexander.c
+++ alexander.c
@@ -18,9 +18,10 @@ alexander (struct presentation *p)
   extern int verbose, quiet;
   struct presentationrule *r;
   struct laurentpoly *determinant;
+  struct laurentpoly2 *determinant2;
   int i, sum, rank, matrixrank;
   int numcols = 0;
-  int gconj;
+  int gconj, gconj2;
 
   topreabelian (p);
 
@@ -42,12 +43,10 @@ alexander (struct presentation *p)
     }
   }
   rank = p->gennum - matrixrank;
-  if (rank > 1)
+  if (rank > 2)
   {
     printf ("Cannot compute Alexander polynomial for groups with rank %d\n", rank);
-//    return (0);
-    rank = 1;
-    matrixrank = p->gennum - rank;
+    return (0);
   }
   if (rank < 1)
   {
@@ -55,6 +54,7 @@ alexander (struct presentation *p)
     return (0);
   }
   gconj = p->gennum;
+  if (rank >= 2) gconj2 = p->gennum - 1;
 
   if (verbose) printf ("Matrix has %d rows and %d columns\n", matrixrank, numcols);
 
@@ -70,12 +70,30 @@ alexander (struct presentation *p)
     return (0);
   }
 
-  determinant = laurent_eliminate_one_indeterminate (p, gconj);
+  switch (rank)
+  {
+    case 1:
+    determinant = laurent_eliminate_one_indeterminate (p, gconj);
+    laurent_canonify (determinant);
+    if (!quiet) printf ("Alexander polynomial (up to t -> 1/t):\n");
+    print_laurentpoly (determinant, 't');
+    printf ("\n");
+    break;
 
-  laurent_canonify (determinant);
-  if (!quiet) printf ("Alexander polynomial (up to t -> 1/t):\n");
-  print_laurentpoly (determinant);
-  printf ("\n");
+    case 2:
+    determinant2 = laurent_eliminate_two_indeterminates (p, gconj2, gconj);
+    //laurent_canonify2 (determinant);
+    printf ("*** Warning: canonization procedure not yet implemented ***\n");
+    if (!quiet) printf ("Alexander polynomial (CAVEAT):\n");
+    print_laurentpoly2 (determinant2, 'u', 'v');
+    printf ("\n");
+    break;
+
+    default:
+    printf ("Software unable to compute Alexander polynomial with these many indeterminates.\n");
+    return (0);
+  }
+
   return (1);
 }
 
@@ -142,7 +160,7 @@ linkingnumber (struct presentation *p)
   if (verbose)
   {
     printf ("Determinant of matrix: ");
-    print_laurentpoly (determinant);
+    print_laurentpoly (determinant, 't');
     printf ("\n");
   }
 
@@ -230,7 +248,7 @@ corank_one_alexander (struct presentation *p)
   if (verbose)
   {
     printf ("Determinant of matrix (before canonization): ");
-    print_laurentpoly (determinant);
+    print_laurentpoly (determinant, 't');
     printf ("\n");
   }
 
@@ -249,13 +267,13 @@ corank_one_alexander (struct presentation *p)
   }
   printf ("Value in 1: %d, deriv = %d\n", val, deriv);
   if (!quiet) printf ("Alexander polynomial (up to t -> 1/t):\n");
-  print_laurentpoly (determinant);
+  print_laurentpoly (determinant, 't');
   printf ("\n");
   return (1);
 }
 
 /*
- *
+ * eliminate one indeterminate by conjugacy
  */
 
 struct laurentpoly *
@@ -312,7 +330,7 @@ laurent_eliminate_one_indeterminate (struct presentation *p, int eliminate)
       {
         matrixcolumn = matrix[j];
         l = matrixcolumn[i];
-        print_laurentpoly (l);
+        print_laurentpoly (l, 't');
         printf ("; \t");
       }
       printf ("\n");
@@ -341,7 +359,95 @@ laurent_eliminate_one_indeterminate (struct presentation *p, int eliminate)
 }
 
 /*
- *
+ * eliminate two indeterminates by conjugacy
+ */
+
+struct laurentpoly2 *
+laurent_eliminate_two_indeterminates (struct presentation *p, int e1, int e2)
+{
+  extern int verbose;
+  struct presentationrule *r;
+  struct laurentpoly2 *l, *determinant;
+  struct laurentpoly2 **matrixcolumn;
+  struct laurentpoly2 ***matrix;
+  int numcols = 0;
+  int numrows;
+  int i, ii, j;
+
+  assert (e1 >= 1 && e2 >= 1);
+  assert (e1 <= p->gennum);
+  assert (e2 <= p->gennum);
+  assert (e1 != e2);
+
+  for (r = p->rules; r; r = r->next) numcols++;
+  numrows = p->gennum - 2;
+  assert (numcols <= numrows);
+
+  if (numcols < numrows) return (0);
+
+  matrix = (struct laurentpoly2 ***) malloc (numcols*sizeof(struct laurentpoly2 **));
+  for (j = 0; j < numcols; j++)
+  {
+    matrix[j] = (struct laurentpoly2 **) malloc (numrows*sizeof(struct laurentpoly2 *));
+  }
+
+  /*
+   * fill matrix
+   */
+
+  for (j = 0, r = p->rules; r; j++, r = r->next)
+  {
+    matrixcolumn = matrix[j];
+    for (i = 1, ii = 0; i <= p->gennum; i++)
+    {
+      if (i == e1 || i == e2) continue;
+      matrixcolumn[ii++] = laurent_get_exp_sum2 (r, i, e1, e2);
+    }
+  }
+
+  /*
+   * stampa della matrice
+   */
+
+  if (verbose)
+  {
+    printf ("Matrix entries:\n");
+    for (i = 0; i < numrows; i++)
+    {
+      for (j = 0; j < numcols; j++)
+      {
+        matrixcolumn = matrix[j];
+        l = matrixcolumn[i];
+        print_laurentpoly2 (l, 'u', 'v');
+        printf ("; \t");
+      }
+      printf ("\n");
+    }
+  }
+
+  determinant = laurent_compute_determinant2 (matrix, numcols);
+
+  /*
+   * free allocated space
+   */
+
+  for (j = 0; j < numcols; j++)
+  {
+    matrixcolumn = matrix[j];
+    for (i = 0; i < numrows; i++)
+    {
+      l = matrixcolumn[i];
+      if (l) free_laurentpoly2 (l);
+    }
+    free (matrixcolumn);
+  }
+  free (matrix);
+
+  return (determinant);
+}
+
+/*
+ * Compute matrix entries for Alexander polynomial computation
  */
 
 struct laurentpoly *
@@ -395,6 +501,82 @@ laurent_get_exp_sum (struct presentationrule *r, int n, int gconj)
 
   assert (l);
   if (laurent_normalize(l) == 0)
+  {
+    free (l);
+    return (0);
+  }
+  assert (l && l->stem[0]);
+  return (l);
+}
+
+/*
+ * Compute matrix entries for Alexander polynomial computation (two indeterminates)
+ */
+
+struct laurentpoly2 *
+laurent_get_exp_sum2 (struct presentationrule *r, int n, int e1, int e2)
+{
+  int k, d;
+  int stemdegree;
+  int re1, re2, minexp1, minexp2, maxexp1, maxexp2;
+  struct laurentpoly2 *l;
+
+  re1 = re2 = 0;
+  minexp1 = minexp2 = INT_MAX;
+  maxexp1 = maxexp2 = INT_MIN;
+  for (k = 0; k < r->length; k++)
+  {
+    if (abs(r->var[k]) == e1)
+    {
+      if (r->var[k] > 0) re1++;
+        else re1--;
+    }
+    if (abs(r->var[k]) == e2)
+    {
+      if (r->var[k] > 0) re2++;
+        else re2--;
+    }
+    if (abs(r->var[k]) == n)
+    {
+      if (re1 < minexp1) minexp1 = re1;
+      if (re2 < minexp2) minexp2 = re2;
+      if (re1 > maxexp1) maxexp1 = re1;
+      if (re2 > maxexp2) maxexp2 = re2;
+    }
+  }
+
+  if (minexp1 > maxexp1 || minexp2 > maxexp2) return (0);
+  stemdegree = maxexp2 - minexp2;
+  l = (struct laurentpoly2 *) malloc (sizeof (struct laurentpoly2)
+            + (stemdegree + 1)*sizeof(struct laurentpoly *));
+  l->denom = 1;
+  l->stemdegree = stemdegree;
+  l->minexpon = minexp2;
+  for (d = 0; d <= stemdegree; d++) l->stem[d] = 0;
+
+  re1 = re2 = 0;
+  for (k = 0; k < r->length; k++)
+  {
+    if (abs(r->var[k]) == e1)
+    {
+      if (r->var[k] > 0) re1++;
+        else re1--;
+    }
+    if (abs(r->var[k]) == e2)
+    {
+      if (r->var[k] > 0) re2++;
+        else re2--;
+    }
+    if (abs(r->var[k]) == n)
+    {
+      d = re2 - minexp2;
+      if (r->var[k] > 0) l->stem[d] = laurentpoly_addmonom (l->stem[d], re1, 1);
+        else l->stem[d] = laurentpoly_addmonom (l->stem[d], re1, -1);
+    }
+  }
+
+  assert (l);
+  if (laurent_normalize2(l) == 0)
   {
     free (l);
     return (0);
@@ -469,6 +651,77 @@ laurent_compute_determinant (struct laurentpoly ***matrix, int n)
 }
 
 /*
+ * compute the determinant of the matrix (two indeterminates)
+ */
+
+struct laurentpoly2 *
+laurent_compute_determinant2 (struct laurentpoly2 ***matrix, int n)
+{
+  int i, ii, jj, i1;
+  int sign;
+  struct laurentpoly2 *determinant = 0, *subdeterminant, *product, *sum;
+  struct laurentpoly2 ***submatrix;
+  struct laurentpoly2 **matrixcol, **submatrixcol, **firstcolumn;
+  struct laurentpoly *lp1;
+
+  assert (n >= 0);
+  if (n == 0)
+  {
+    determinant = (struct laurentpoly2 *) malloc (sizeof (struct laurentpoly2) + sizeof (struct laurentpoly *));
+    determinant->denom = 1;
+    determinant->minexpon = 0;
+    determinant->stemdegree = 0;
+    lp1 = (struct laurentpoly *) malloc (sizeof (struct laurentpoly) + sizeof (int));
+    determinant->stem[0] = lp1;
+    lp1->denom = 1;
+    lp1->minexpon = 0;
+    lp1->stemdegree = 0;
+    lp1->stem[0] = 1;
+    return (determinant);
+  }
+  if (n == 1) return (laurent_dup2(matrix[0][0]));
+
+  /*
+   * developping the determinant about the first column
+   */
+
+  submatrix = (struct laurentpoly2 ***) malloc ( (n-1)*sizeof (struct laurentpoly2 **) );
+  for (jj = 0; jj < n-1; jj++)
+    submatrix[jj] = (struct laurentpoly2 **) malloc ( (n-1)*sizeof (struct laurentpoly2 *) );
+
+  firstcolumn = matrix[0];
+  sign = 1;
+  for (i = 0, sign = 1; i < n; i++, sign = -sign)
+  {
+    if (firstcolumn[i] == 0) continue;
+    for (jj = 0; jj < n - 1; jj++)
+    {
+      submatrixcol = submatrix[jj];
+      matrixcol = matrix[jj + 1];
+      for (i1 = 0, ii = 0; i1 < n; i1++)
+      {
+        if (i1 == i) continue;
+        submatrixcol[ii++] = matrixcol[i1];
+      }
+    }
+
+    subdeterminant = laurent_compute_determinant2 (submatrix, n-1);
+    product = laurent_mul2 (subdeterminant, firstcolumn[i]);
+    if (subdeterminant) free_laurentpoly2 (subdeterminant);
+    if (sign < 0) laurent_negate2 (product);
+    sum = laurent_add2 (determinant, product);
+    if (product && product != sum) free_laurentpoly2 (product);
+    if (determinant && sum != determinant) free_laurentpoly2 (determinant);
+    determinant = sum;
+  }
+
+  for (jj = 0; jj < n-1; jj++) free (submatrix[jj]);
+  free (submatrix);
+
+  return (determinant);
+}
+
+/*
  * negate polynomial
  */
 
@@ -479,6 +732,21 @@ laurent_negate (struct laurentpoly *term)
 
   if (term == 0) return;
   for (i = 0; i <= term->stemdegree; i++) term->stem[i] = -term->stem[i];
+
+  return;
+}
+
+/*
+ * negate polynomial (two indeterminates)
+ */
+
+void
+laurent_negate2 (struct laurentpoly2 *term)
+{
+  int i;
+
+  if (term == 0) return;
+  for (i = 0; i <= term->stemdegree; i++) laurent_negate (term->stem[i]);
 
   return;
 }
@@ -564,6 +832,30 @@ laurent_dup (struct laurentpoly *l)
 }
 
 /*
+ * duplicate a laurent polynomial (two indeterminates)
+ */
+
+struct laurentpoly2 *
+laurent_dup2 (struct laurentpoly2 *l)
+{
+  int k;
+  struct laurentpoly2 *res;
+
+  if (l == 0) return (0);
+
+  res = (struct laurentpoly2 *) malloc (sizeof (struct laurentpoly2) +
+            (l->stemdegree + 1)*sizeof(struct laurentpoly *));
+
+  res->denom = l->denom;
+  res->minexpon = l->minexpon;
+  res->stemdegree = l->stemdegree;
+
+  for (k = 0; k <= l->stemdegree; k++) res->stem[k] = laurent_dup (l->stem[k]);
+
+  return (res);
+}
+
+/*
  * add two laurent polynomials
  */
 
@@ -607,6 +899,56 @@ laurent_add (struct laurentpoly *a1, struct laurentpoly *a2)
 }
 
 /*
+ * add two laurent polynomials (two indeterminates)
+ */
+
+struct laurentpoly2 *
+laurent_add2 (struct laurentpoly2 *a1, struct laurentpoly2 *a2)
+{
+  int minexp, maxexp, k;
+  struct laurentpoly2 *res;
+  struct laurentpoly *addres;
+
+  if (a1 == 0) return (laurent_dup2(a2));
+  if (a2 == 0) return (laurent_dup2(a1));
+
+  minexp = a1->minexpon;
+  if (a2->minexpon < minexp) minexp = a2->minexpon;
+
+  maxexp = a1->minexpon + a1->stemdegree;
+  if (a2->minexpon + a2->stemdegree > maxexp) maxexp = a2->minexpon + a2->stemdegree;
+
+  res = (struct laurentpoly2 *) malloc (sizeof (struct laurentpoly2) +
+           (maxexp - minexp + 1)*sizeof(struct laurentpoly *));
+
+  assert (a1->denom == a2->denom);
+  res->denom = a1->denom;
+  res->minexpon = minexp;
+  res->stemdegree = maxexp - minexp;
+
+  for (k = 0; k <= res->stemdegree; k++) res->stem[k] = 0;
+
+  for (k = 0; k <= a1->stemdegree; k++) res->stem[k + a1->minexpon - minexp] = laurent_dup (a1->stem[k]);
+  for (k = 0; k <= a2->stemdegree; k++)
+  {
+    if (a2->stem[k] == 0) continue;
+    addres = laurent_add (res->stem[k + a2->minexpon - minexp], a2->stem[k]);
+    if (res->stem[k + a2->minexpon - minexp]) free (res->stem[k + a2->minexpon - minexp]);
+    res->stem[k + a2->minexpon - minexp] = addres;
+  }
+
+  /* normalize */
+
+  if (res && laurent_normalize2 (res) == 0)
+  {
+    free (res);
+    res = 0;
+  }
+
+  return (res);
+}
+
+/*
  * normalize a laurent polynomial
  * such that first and last element in
  * stem are nonzero
@@ -616,6 +958,32 @@ laurent_add (struct laurentpoly *a1, struct laurentpoly *a2)
 
 struct laurentpoly *
 laurent_normalize (struct laurentpoly *l)
+{
+  int k;
+
+  if (l == 0) return (0);
+
+  while (l->stem[0] == 0)
+  {
+    if (l->stemdegree == 0)
+    {
+      return (0);
+    }
+    for (k = 0; k < l->stemdegree; k++) l->stem[k] = l->stem[k+1];
+    l->stemdegree--;
+    l->minexpon++;
+  }
+
+  while (l->stem[l->stemdegree] == 0)
+  {
+    assert (l->stemdegree > 0);
+    l->stemdegree--;
+  }
+  return (l);
+}
+
+struct laurentpoly2 *
+laurent_normalize2 (struct laurentpoly2 *l)
 {
   int k;
 
@@ -675,11 +1043,50 @@ laurent_mul (struct laurentpoly *f1, struct laurentpoly *f2)
 }
 
 /*
+ * multiply two laurent polynomials (two indeterminates)
+ */
+
+struct laurentpoly2 *
+laurent_mul2 (struct laurentpoly2 *f1, struct laurentpoly2 *f2)
+{
+  int i, j;
+  int resstemdegree;
+  struct laurentpoly2 *res;
+  struct laurentpoly *mulres, *addres;
+
+  if (f1 == 0 || f2 == 0) return (0);
+
+  resstemdegree = f1->stemdegree + f2->stemdegree;
+
+  res = (struct laurentpoly2 *) malloc (sizeof (struct laurentpoly2) +
+          (resstemdegree + 1)*sizeof (struct laurentpoly *));
+  res->denom = f1->denom * f2->denom;
+  res->minexpon = f1->minexpon + f2->minexpon;
+  res->stemdegree = resstemdegree;
+
+  for (i = 0; i <= resstemdegree; i++) res->stem[i] = 0;
+
+  for (i = 0; i <= f1->stemdegree; i++)
+  {
+    for (j = 0; j <= f2->stemdegree; j++)
+    {
+      mulres = laurent_mul (f1->stem[i], f2->stem[j]);
+      addres = laurent_add (res->stem[i + j], mulres);
+      if (mulres) free (mulres);
+      if (res->stem[i + j]) free (res->stem[i + j]);
+      res->stem[i + j] = addres;
+    }
+  }
+
+  return (res);
+}
+
+/*
  * print a Laurent polynomial
  */
 
 void
-print_laurentpoly (struct laurentpoly *l)
+print_laurentpoly (struct laurentpoly *l, char indet)
 {
   int i, expon;
 
@@ -700,7 +1107,7 @@ print_laurentpoly (struct laurentpoly *l)
       }
       if (expon != 0)
       {
-        printf ("t");
+        printf ("%c", indet);
         if (expon != 1)
         {
           if (expon > 0) printf ("^%d", expon);
@@ -709,4 +1116,93 @@ print_laurentpoly (struct laurentpoly *l)
       }
     }
   }
+}
+
+/*
+ * print a Laurent polynomial with two indeterminates
+ */
+
+void
+print_laurentpoly2 (struct laurentpoly2 *l, char indet1, char indet2)
+{
+  int i, expon;
+
+  if (l == 0) {printf ("0"); return;}
+  assert (l->denom == 1);
+  assert (l->stem[0]);
+  for (i = 0; i <= l->stemdegree; i++)
+  {
+    expon = i + l->minexpon;
+    if (l->stem[i])
+    {
+      printf ("+ [");
+      print_laurentpoly (l->stem[i], indet1);
+      printf ("]");
+      if (expon != 0)
+      {
+        printf ("%c", indet2);
+        if (expon != 1)
+        {
+          if (expon > 0) printf ("^%d", expon);
+            else printf ("^(%d)", expon);
+        }
+      }
+    }
+  }
+}
+
+/*
+ * free data associated to a laurent polynomial with 2 indeterminates
+ */
+
+void
+free_laurentpoly2 (struct laurentpoly2 *l)
+{
+  int i;
+
+  if (l == 0) return;
+
+  for (i = 0; i <= l->stemdegree; i++)
+  {
+    if (l->stem[i]) free (l->stem[i]);
+  }
+  free (l);
+}
+
+/*
+ * add a monomial to a laurent_poly
+ */
+
+struct laurentpoly *
+laurentpoly_addmonom (struct laurentpoly *l, int expon, int coef)
+{
+  struct laurentpoly *m = 0;
+  struct laurentpoly *res;
+
+  if (coef == 0) return (l);
+
+  if (l && expon >= l->minexpon && expon <= l->minexpon + l->stemdegree)
+  {
+    l->stem[expon - l->minexpon] += coef;
+    if (laurent_normalize(l) == 0)
+    {
+      free (l);
+      return (0);
+    }
+    return (l);
+  }
+
+  m = (struct laurentpoly *) malloc (sizeof (struct laurentpoly) + sizeof (int));
+  m->denom = 1;
+  m->minexpon = expon;
+  m->stemdegree = 0;
+  m->stem[0] = coef;
+
+  if (l == 0) return (m);
+
+  res = laurent_add (l, m);
+  free (m);
+  free (l);
+
+  return (res);
 }
--- alexander.h
+++ alexander.h
@@ -9,19 +9,37 @@ struct laurentpoly {
   int stem[];
 };
 
+struct laurentpoly2 {
+  int minexpon;
+  int stemdegree;
+  int denom;
+  struct laurentpoly *stem[];
+};
+
 /* prototypes */
 
 int alexander (struct presentation *p);
 int linkingnumber (struct presentation *p);
 int corank_one_alexander (struct presentation *p);
 struct laurentpoly *laurent_eliminate_one_indeterminate (struct presentation *p, int eliminate);
+struct laurentpoly2 *laurent_eliminate_two_indeterminates (struct presentation *p, int e1, int e2);
 struct laurentpoly *laurent_get_exp_sum (struct presentationrule *r, int g, int gconj);
+struct laurentpoly2 *laurent_get_exp_sum2 (struct presentationrule *r, int g, int e1, int e2);
 struct laurentpoly *laurent_compute_determinant (struct laurentpoly ***matrix, int n);
-void print_laurentpoly (struct laurentpoly *l);
+struct laurentpoly2 *laurent_compute_determinant2 (struct laurentpoly2 ***matrix, int n);
+void print_laurentpoly (struct laurentpoly *l, char indet);
+void print_laurentpoly2 (struct laurentpoly2 *l, char indet1, char indet2);
 struct laurentpoly *laurent_add (struct laurentpoly *add1, struct laurentpoly *add2);
+struct laurentpoly2 *laurent_add2 (struct laurentpoly2 *add1, struct laurentpoly2 *add2);
 struct laurentpoly *laurent_mul (struct laurentpoly *fact1, struct laurentpoly *fact2);
+struct laurentpoly2 *laurent_mul2 (struct laurentpoly2 *fact1, struct laurentpoly2 *fact2);
 void laurent_negate (struct laurentpoly *term);
+void laurent_negate2 (struct laurentpoly2 *term);
 struct laurentpoly *laurent_normalize (struct laurentpoly *l);
+struct laurentpoly2 *laurent_normalize2 (struct laurentpoly2 *l);
 struct laurentpoly *laurent_dup (struct laurentpoly *l);
+struct laurentpoly2 *laurent_dup2 (struct laurentpoly2 *l);
 void laurent_canonify (struct laurentpoly *l);
 void laurent_t_to_oneovert (struct laurentpoly *l);
+void free_laurentpoly2 (struct laurentpoly2 *l);
+struct laurentpoly *laurentpoly_addmonom (struct laurentpoly *l, int expon, int coef);
