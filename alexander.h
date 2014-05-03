0a17701e31651b97fbcd75a5c6cb1bae98185c2e
Maurizio Paolini <maurizio.paolini@unicatt.it>
2014-05-03 18:49:13 +0000

    Forse sistemato baco concettuale.  Purtroppo il calcolo dell'ideale non c'e' e forse non ci sara' mai nel caso di contributi extra!

--- alexander.h
+++ alexander.h
@@ -22,11 +22,16 @@ int alexander (struct presentation *p);
 int linkingnumber (struct presentation *p);
 int corank_one_alexander (struct presentation *p);
 struct laurentpoly *laurent_eliminate_one_indeterminate (struct presentation *p, int eliminate);
-struct laurentpoly2 *laurent_eliminate_two_indeterminates (struct presentation *p, int e1, int e2);
+struct laurentpoly2 *laurent_eliminate_two_indeterminates (struct presentation *p, int e1, int e2,
+                      struct laurentpoly2 ***extrapt);
 struct laurentpoly *laurent_get_exp_sum (struct presentationrule *r, int g, int gconj);
 struct laurentpoly2 *laurent_get_exp_sum2 (struct presentationrule *r, int g, int e1, int e2);
+struct laurentpoly2 *laurent_mixed_derivative2 (struct presentationrule *r, int x1, int x2);
+struct laurentpoly2 *laurent_common_factor2 (struct presentationrule *r, int x1, int x2);
 struct laurentpoly *laurent_compute_determinant (struct laurentpoly ***matrix, int n);
 struct laurentpoly2 *laurent_compute_determinant2 (struct laurentpoly2 ***matrix, int n);
+struct laurentpoly2 *laurent_minor_determinant2 (struct laurentpoly2 ***matrix, int n,
+                          struct laurentpoly2 **extrarow, int row_to_substitute);
 void print_laurentpoly (struct laurentpoly *l, char indet);
 void print_laurentpoly2 (struct laurentpoly2 *l, char indet1, char indet2);
 struct laurentpoly *laurent_add (struct laurentpoly *add1, struct laurentpoly *add2);
@@ -44,3 +49,7 @@ void laurent_canonify2 (struct laurentpoly2 *l);
 void laurent_t_to_oneovert (struct laurentpoly *l);
 void free_laurentpoly2 (struct laurentpoly2 *l);
 struct laurentpoly *laurentpoly_addmonom (struct laurentpoly *l, int expon, int coef);
+
+int laurent_sum_coefficients (struct laurentpoly *l);
+struct laurentpoly *laurent_sum_coefficients2 (struct laurentpoly2 *l);
+struct laurentpoly *laurent_sum_each_coefficient2 (struct laurentpoly2 *l);
