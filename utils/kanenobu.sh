9b2d23dd897454ce03070723b2c9494ed093787f
Maurizio Paolini <maurizio.paolini@unicatt.it>
2016-05-04 07:21:02 +0000

    generate kanenobu knots

---
+++ utils/kanenobu.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# generate kanenobu knot for given p and q
+#
+# see http://www.ams.org/journals/proc/1986-097-01/S0002-9939-1986-0831406-7/S0002-9939-1986-0831406-7.pdf
+
+p=$1
+q=$2
+
+if [ -z "$p" ]
+then
+  echo "usage: $0 p q"
+  exit 1
+fi
+
+xp='x'
+xq='x'
+
+if [ "$p" -lt 0 ]
+then
+  xp='X'
+  p=$[ - $p ]
+fi
+if [ "$q" -lt 0 ]
+then
+  xq='X'
+  q=$[ - $q ]
+fi
+
+echo "knot {"
+echo "        ^       ;"
+echo "  |   ^   ^   | ;"
+
+for i in `seq 1 $p`
+do
+  echo "  |  |  $xp  |  | ;"
+done
+
+echo " | ^ || ^ || ^ |;"
+echo "  X X X   x x x ;"
+echo " | U |X   x| U |;"
+echo " |   U | | U   |;"
+
+for i in `seq 1 $q`
+do
+  echo "  |     $xq     | ;"
+done
+
+echo "    U       U   ;"
+echo "}"
+
