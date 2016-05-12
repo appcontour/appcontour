1650753e84bc5fb9619c75adee6245cd78aea620
Maurizio Paolini <maurizio.paolini@unicatt.it>
2016-05-12 15:00:44 +0000

    fix kanenobu p, q interpretation error

--- utils/kanenobu.sh
+++ utils/kanenobu.sh
@@ -6,19 +6,31 @@
 #
 # entries in knot atlas with same Alexander polynomial are (up to 10 crossings):
 #
-# 8_8 10_129 10_137
-# altri nodi con al piu' 10 incroci: 8_9, 10_155(?)
+# p = q = 0 -> 4_1 # 4_1
+# p = 0, q = 1 -> 10_137 (or it's mirror image)
+#
+# in caso di "half" twists ci sono anche i nodi 8_8(?) 8_9 10_129 10_155
 #
 
 p=$1
 q=$2
+halftwists="$3"
 
 if [ -z "$p" ]
 then
-  echo "usage: $0 p q"
+  echo "usage: $0 p q [halftwists]"
+  echo ""
+  echo "$0 1 1"
+  echo " is the same as"
+  echo "$0 2 2 halftwists"
   exit 1
 fi
 
+if [ -z "$halftwists" ]
+then
+  p=$[ 2 * $p ]
+  q=$[ 2 * $q ]
+fi
 xp='x'
 xq='x'
 
