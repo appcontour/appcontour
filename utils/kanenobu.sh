c4f27173ffb723e768937c23c1ae42aa8ae6b3d9
Maurizio Paolini <maurizio.paolini@unicatt.it>
2016-05-15 23:16:08 +0000

    revert to halftwists Kanenobu

--- utils/kanenobu.sh
+++ utils/kanenobu.sh
@@ -1,47 +1,54 @@
 #!/bin/bash
 #
-# generate kanenobu knot for given p and q
+# generate kanenobu knot K(p,q) for given p and q
 #
-# see http://www.ams.org/journals/proc/1986-097-01/S0002-9939-1986-0831406-7/S0002-9939-1986-0831406-7.pdf
+# [ref: Taizo Kanenobu, Exampes on Polynoomial Invariants of Knots and Links,
+# Math. Ann. 275, 555-572 (1986)]
+#
+# see also http://www.ams.org/journals/proc/1986-097-01/S0002-9939-1986-0831406-7/S0002-9939-1986-0831406-7.pdf
+# where K_{p,q} is K(-2p,-2q)
 #
 # entries in knot atlas with same Alexander polynomial are (up to 10 crossings):
 #
 # p = q = 0 -> 4_1 # 4_1
-# p = 0, q = 1 -> 10_137 (or it's mirror image)
+# p = 0, q = 2 -> 10_137 (or it's mirror image)
 #
 # in caso di "half" twists ci sono anche i nodi 8_8(?) 8_9 10_129 10_155
 #
 
 p=$1
 q=$2
-halftwists="$3"
+completetwists="$3"
 
 if [ -z "$p" ]
 then
-  echo "usage: $0 p q [halftwists]"
+  echo "usage: $0 p q [completetwists]"
   echo ""
-  echo "$0 1 1"
+  echo "$0 1 1 completetwists"
   echo " is the same as"
-  echo "$0 2 2 halftwists"
+  echo "$0 2 2"
   exit 1
 fi
 
-if [ -z "$halftwists" ]
+if [ -n "$completetwists" ]
 then
+#
+# should also change sign
+#
   p=$[ 2 * $p ]
   q=$[ 2 * $q ]
 fi
-xp='x'
-xq='x'
+xp='X'
+xq='X'
 
 if [ "$p" -lt 0 ]
 then
-  xp='X'
+  xp='x'
   p=$[ - $p ]
 fi
 if [ "$q" -lt 0 ]
 then
-  xq='X'
+  xq='x'
   q=$[ - $q ]
 fi
 
