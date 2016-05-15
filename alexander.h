580a95df1a9c37ddc1a7a6370f8bf705ce776615
Maurizio Paolini <maurizio.paolini@unicatt.it>
2016-05-15 23:57:19 +0000

    split functions related to laurent polynomials into a different file

--- alexander.h
+++ alexander.h
@@ -2,18 +2,6 @@
  * definitions for alexander polynomial
  */
 
-struct laurentpoly {
-  int minexpon;
-  int stemdegree;
-  int stem[];
-};
-
-struct laurentpoly2 {
-  int minexpon;
-  int stemdegree;
-  struct laurentpoly *stem[];
-};
-
 #define IDEAL_MAX_GENERATORS_NUM 10
 
 /*
@@ -70,35 +58,11 @@ struct laurentpoly *laurent_compute_determinant (struct laurentpoly ***matrix, i
 struct laurentpoly2 *laurent_compute_determinant2 (struct laurentpoly2 ***matrix, int n);
 struct laurentpoly2 *laurent_minor_determinant2 (struct laurentpoly2 ***matrix, int n,
                           int row_to_substitute);
-void print_laurentpoly (struct laurentpoly *l, char indet);
-void print_laurentpoly2 (struct laurentpoly2 *l, char indet1, char indet2);
-struct laurentpoly *laurent_add (struct laurentpoly *add1, struct laurentpoly *add2);
-struct laurentpoly2 *laurent_add2 (struct laurentpoly2 *add1, struct laurentpoly2 *add2);
-struct laurentpoly *laurent_mul (struct laurentpoly *fact1, struct laurentpoly *fact2);
-struct laurentpoly2 *laurent_mul2 (struct laurentpoly2 *fact1, struct laurentpoly2 *fact2);
-void laurent_negate (struct laurentpoly *term);
-void laurent_negate2 (struct laurentpoly2 *term);
-struct laurentpoly *laurent_normalize (struct laurentpoly *l);
-struct laurentpoly2 *laurent_normalize2 (struct laurentpoly2 *l);
-struct laurentpoly *laurent_dup (struct laurentpoly *l);
-struct laurentpoly2 *laurent_dup2 (struct laurentpoly2 *l);
-void laurent_canonify (struct laurentpoly *l);
-void laurent_canonify2 (struct laurentpoly2 *l);
 int canonify_ideal2 (struct laurentpoly2 **lpt, struct laurentpoly2 **extradets, int extranum);
-void laurent_t_to_oneovert (struct laurentpoly *l);
-void free_laurentpoly2 (struct laurentpoly2 *l);
-struct laurentpoly *laurentpoly_addmonom (struct laurentpoly *l, int expon, int coef);
-struct laurentpoly2 *laurentpoly2_addmonom (struct laurentpoly2 *l, int expu, int expv, int coef);
 struct laurentmatrix *laurent_build_matrix (struct presentation *p, int eliminate);
 void laurent_free_matrix (struct laurentmatrix *matrix);
-struct laurentpoly *laurent_gcd (struct laurentpoly *p1, struct laurentpoly *p2);
-int laurent_factor_content (struct laurentpoly *p);
-struct laurentpoly *laurent_euclid (struct laurentpoly *p1, struct laurentpoly *p2);
 void laurent_simplify_ideal (struct alexanderideal *ai);
 
-int laurent_sum_coefficients (struct laurentpoly *l);
-struct laurentpoly *laurent_sum_coefficients2 (struct laurentpoly2 *l);
-struct laurentpoly *laurent_sum_each_coefficient2 (struct laurentpoly2 *l);
 int laurent_suppdim2 (struct laurentpoly2 *l);
 void laurent_getthree2 (struct laurentpoly2 *l, int *x1, int *x2, int *x3);
 int base_canonify2 (struct laurentpoly2 **lpt);
@@ -111,7 +75,6 @@ int laurent2_lexicocompare (struct laurentpoly2 *p1, struct laurentpoly2 *p2);
 void laurent_canonifysign (struct laurentpoly *p);
 void laurent2_canonifysign (struct laurentpoly2 *p);
 
-int gcd (int a, int b);
 void base_random (int matrixb[2][2]);
 int isinvertible_base (int b[2][2]);
 
---
+++ laurent.h
@@ -0,0 +1,46 @@
+/*
+ * definitions for alexander polynomial
+ */
+
+struct laurentpoly {
+  int minexpon;
+  int stemdegree;
+  int stem[];
+};
+
+struct laurentpoly2 {
+  int minexpon;
+  int stemdegree;
+  struct laurentpoly *stem[];
+};
+
+void laurent_negate (struct laurentpoly *term);
+void laurent_negate2 (struct laurentpoly2 *term);
+void print_laurentpoly (struct laurentpoly *l, char indet);
+void print_laurentpoly2 (struct laurentpoly2 *l, char indet1, char indet2);
+struct laurentpoly *read_laurentpoly (FILE *file, char indet_names[2]);
+struct laurentpoly2 *read_laurentpoly2 (FILE *file, char indet_names[2]);
+struct laurentpoly *laurent_add (struct laurentpoly *add1, struct laurentpoly *add2);
+struct laurentpoly2 *laurent_add2 (struct laurentpoly2 *add1, struct laurentpoly2 *add2);
+struct laurentpoly *laurent_mul (struct laurentpoly *fact1, struct laurentpoly *fact2);
+struct laurentpoly2 *laurent_mul2 (struct laurentpoly2 *fact1, struct laurentpoly2 *fact2);
+struct laurentpoly *laurent_normalize (struct laurentpoly *l);
+struct laurentpoly2 *laurent_normalize2 (struct laurentpoly2 *l);
+struct laurentpoly *laurent_dup (struct laurentpoly *l);
+struct laurentpoly2 *laurent_dup2 (struct laurentpoly2 *l);
+void laurent_canonify (struct laurentpoly *l);
+void laurent_canonify2 (struct laurentpoly2 *l);
+void laurent_t_to_oneovert (struct laurentpoly *l);
+void free_laurentpoly2 (struct laurentpoly2 *l);
+struct laurentpoly *laurentpoly_addmonom (struct laurentpoly *l, int expon, int coef);
+struct laurentpoly2 *laurentpoly2_addmonom (struct laurentpoly2 *l, int expu, int expv, int coef);
+int laurent_sum_coefficients (struct laurentpoly *l);
+struct laurentpoly *laurent_sum_coefficients2 (struct laurentpoly2 *l);
+struct laurentpoly *laurent_sum_each_coefficient2 (struct laurentpoly2 *l);
+
+struct laurentpoly *laurent_gcd (struct laurentpoly *p1, struct laurentpoly *p2);
+int laurent_factor_content (struct laurentpoly *p);
+struct laurentpoly *laurent_euclid (struct laurentpoly *p1, struct laurentpoly *p2);
+
+int gcd (int a, int b);
+
