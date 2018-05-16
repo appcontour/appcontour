b94c567046eaaed237a7e7451cfc60364dea5d61
Maurizio Paolini <maurizio.paolini@unicatt.it>
2018-05-16 13:44:02 +0000

    almeno compila...

--- groebner.c
+++ groebner.c
@@ -6,6 +6,7 @@
 #include <assert.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "laurent.h"
 #include "alexander.h"
 #include "groebner.h"
@@ -15,6 +16,7 @@ groebner1 (struct alexanderideal *ai)
 {
   int i, reductions, newgenerators;
   extern int experimental;
+  struct stemideal *si;
 
   if (ai->indets < 1) return (ai);
   assert (ai->indets == 1);
@@ -28,27 +30,30 @@ groebner1 (struct alexanderideal *ai)
       printf ("\n");
     }
   }
+
+  si = ai2si (ai);
   while (1)
   {
-    reductions = groebner1_tryreduce (ai->l, ai->l1num);
+    reductions = groebner1_tryreduce (si);
 if (experimental)
 {
   printf ("Performed %d reductions\n", reductions);
-  printout_ideal1 (ai, 0);
+  printout_si (si);
 }
     assert (reductions >= 0);
-    if (experimental || reductions > 0) ai->l1num -= groebner1_dropzeros (ai->l, ai->l1num);
+    if (experimental || reductions > 0) groebner1_dropzeros (si);
 if (experimental)
 {
   printf ("after zero polynomials drop\n");
-  printout_ideal1 (ai, 0);
+  printout_si (si);
 }
-    newgenerators = groebner1_add_spolynomials (ai->l, ai->l1num, ai->max_generators_num);
+    newgenerators = groebner1_add_spolynomials (si);
     assert (newgenerators >= 0);
-    if (newgenerators > 0) ai->l1num += newgenerators;
     if (reductions + newgenerators <= 0) break;
   }
 
+  ai = si2ai (si, ai);
+  free_stemideal (si);
   return (ai);
 }
 
@@ -57,21 +62,22 @@ if (experimental)
  */
 
 int
-groebner1_dropzeros (struct laurentpoly *l[], int lnum)
+groebner1_dropzeros (struct stemideal *si)
 {
   int i, j;
   extern int verbose, experimental;
 
-  for (i = 0, j = 0; i < lnum; i++)
+  for (i = 0, j = 0; i < si->num; i++)
   {
-    if (l[i] != 0)
+    if (si->stem[i] != 0)
     {
-      l[j++] = l[i];
+      si->stem[j++] = si->stem[i];
     } else {
       if (verbose) printf ("Ideal simplification: zero polynomial removed\n");
     }
   }
   if (experimental) printf ("i - j = %d\n", i - j);
+  si->num = j;
   return (i - j);
 }
 
@@ -80,14 +86,14 @@ groebner1_dropzeros (struct laurentpoly *l[], int lnum)
  */
 
 int
-groebner1_tryreduce (struct laurentpoly *l[], int lnum)
+groebner1_tryreduce (struct stemideal *si)
 {
   int reduce, totreduce;
 
   totreduce = 0;
   do
   {
-    reduce = groebner1_tryreduce_once (l, lnum);
+    reduce = groebner1_tryreduce_once (si);
     totreduce += reduce;
   } while (reduce > 0);
 
@@ -99,20 +105,20 @@ groebner1_tryreduce (struct laurentpoly *l[], int lnum)
  */
 
 int
-groebner1_tryreduce_once (struct laurentpoly *l[], int lnum)
+groebner1_tryreduce_once (struct stemideal *si)
 {
   int i, j, retcode;
   int reductions = 0;
 
-  for (i = 0; i < lnum; i++)
+  for (i = 0; i < si->num; i++)
   {
-    if (l[i] == 0) continue;
-    assert (l[i]->stemdegree >= 0);
+    if (si->stem[i] == 0) continue;
+    assert (si->stem[i]->degree >= 0);
     /* use i-th polynomial to try and reduce all others */
-    for (j = 0; j < lnum; j++)
+    for (j = 0; j < si->num; j++)
     {
       if (i == j) continue;
-      l[j] = groebner1_reduce_using_rule (l[j], l[i], &retcode);
+      si->stem[j] = groebner1_reduce_using_rule (si->stem[j], si->stem[i], &retcode);
       if (retcode > 0) reductions += retcode;
     }
   }
@@ -123,24 +129,24 @@ groebner1_tryreduce_once (struct laurentpoly *l[], int lnum)
  * reduce a laurent polynomial using another laurent polynomial
  */
 
-struct laurentpoly *
-groebner1_reduce_using_rule (struct laurentpoly *p, struct laurentpoly *rule, int *statuspt)
+struct stem *
+groebner1_reduce_using_rule (struct stem *p, struct stem *rule, int *statuspt)
 {
   int j, ruledegree;
 
   *statuspt = 0;
-  assert (rule && rule->stemdegree >= 0);
+  assert (rule && rule->degree >= 0);
   if (p == 0) return (0);
-  ruledegree = rule->stemdegree;
+  ruledegree = rule->degree;
 
-  for (j = p->stemdegree; j >= ruledegree; j--)
+  for (j = p->degree; j >= ruledegree; j--)
   {
     printf ("RULE higher: NOT IMPLEMENTED\n");
   }
 
   assert ( *statuspt == 0 );  // TODO reminder, here we MUST canonify stem
 
-  for (j = 0; p->stemdegree >= ruledegree + j; j++)
+  for (j = 0; p->degree >= ruledegree + j; j++)
   {
     printf ("RULE lower: NOT IMPLEMENTED\n");
   }
@@ -154,8 +160,93 @@ groebner1_reduce_using_rule (struct laurentpoly *p, struct laurentpoly *rule, in
  */
 
 int
-groebner1_add_spolynomials (struct laurentpoly *l[], int lnum, int ldim)
+groebner1_add_spolynomials (struct stemideal *si)
 {
   printf ("ADD-S-POLYNOMIALS NOT IMPLEMENTED\n");
   return (0);
+}
+
+/*
+ * convert from alexander ideal to the simpler stemideal
+ */
+
+struct stemideal *
+ai2si (struct alexanderideal *ai)
+{
+  struct stemideal *si;
+  int i;
+  int numalloc = ai->l1num + GB_EXTRAROOM(ai->l1num);
+
+  si = (struct stemideal *) malloc (STEMIDEALSIZE(numalloc));
+  si->dim = numalloc;
+  si->num = ai->l1num;
+  for (i = 0; i < ai->l1num; i++) si->stem[i] = lp2stem (ai->l[i]);
+
+  return (si);
+}
+
+struct stem *
+lp2stem (struct laurentpoly *lp)
+{
+  struct stem *stem;
+  int i;
+
+  stem = (struct stem *) malloc (STEMSIZE(lp->stemdegree + 1));
+  stem->dim = lp->stemdegree + 1;
+  stem->degree = lp->stemdegree;
+
+  for (i = 0; i <= lp->stemdegree; i++) stem->coef[i] = lp->stem[i].l0;
+
+  return (stem);
+}
+
+struct alexanderideal *
+si2ai (struct stemideal *si, struct alexanderideal *ai)
+{
+  int i;
+
+  assert (ai->max_generators_num >= si->num);
+
+  for (i = 0; i < ai->l1num; i++) {free_laurentpoly (ai->l[i]); ai->l[i] = 0;}
+  for (i = 0; i < si->num; i++)
+  {
+    ai->l[i] = stem2lp (si->stem[i]);
+  }
+  ai->l1num = si->num;
+  return (ai);
+}
+
+struct laurentpoly *
+stem2lp (struct stem *stem)
+{
+  int i;
+  struct laurentpoly *lp;
+
+  lp = (struct laurentpoly *) malloc (POLYSIZE (stem->degree + 1));
+  lp->stemdegree = stem->degree;
+  lp->minexpon = 0;
+  lp->indets = 1;
+  for (i = 0; i <= stem->degree; i++) lp->stem[i].l0 = stem->coef[i];
+
+  return (lp);
+}
+
+void
+free_stemideal (struct stemideal *si)
+{
+  int i;
+
+  for (i = 0; i < si->num; i++)
+  {
+    assert (si->stem[i]);
+    free (si->stem[i]);
+  }
+
+  free (si);
+}
+
+void
+printout_si (struct stemideal *si)
+{
+  printf ("printout_si non implemented\n");
 }
--- groebner.h
+++ groebner.h
@@ -2,29 +2,7 @@
   #include "fundamental.h"   // needed for structure definition
 #endif
 
-/*
- * prototypes
- */
-
-/* laurent polynomials in one indeterminate (integral coefficient */
-
-struct alexanderideal * groebner1 (struct alexanderideal *ai);
-struct stemideal * ai2si (struct alexanderideal ai);
-struct alexanderideal * si2ai (struct stemideal *si, struct alexanderideal *ai);
-int groebner1_tryreduce (struct laurentpoly *l[], int gennum);
-int groebner1_tryreduce_once (struct laurentpoly *l[], int gennum);
-struct laurentpoly * groebner1_reduce_using_rule (struct laurentpoly *p,
-                                                  struct laurentpoly *rule, int *retcode);
-int groebner1_dropzeros (struct laurentpoly *l[], int gennum);
-int groebner1_add_spolynomials (struct laurentpoly *l[], int gennum, int gendim);
-
-/*
- * struct for working with stems in one indeterminate
- * TODO we should eventually be able to deal with integers of any size...
- */
-
 typedef int Stemint;
-#define STEMSIZE(n) (sizeof(struct stem) + (n)*sizeof(Stemint))
 
 struct stem {
   int dim;
@@ -37,3 +15,33 @@ struct stemideal {
   int num;
   struct stem *stem[];
 };
+
+/*
+ * prototypes
+ */
+
+/* laurent polynomials in one indeterminate (integral coefficient */
+
+struct alexanderideal * groebner1 (struct alexanderideal *ai);
+struct stemideal * ai2si (struct alexanderideal *ai);
+struct stem * lp2stem (struct laurentpoly *lp);
+struct alexanderideal * si2ai (struct stemideal *si, struct alexanderideal *ai);
+struct laurentpoly * stem2lp (struct stem *stem);
+int groebner1_tryreduce (struct stemideal *si);
+int groebner1_tryreduce_once (struct stemideal *si);
+struct stem * groebner1_reduce_using_rule (struct stem *p,
+                                                  struct stem *rule, int *retcode);
+int groebner1_dropzeros (struct stemideal *si);
+int groebner1_add_spolynomials (struct stemideal *si);
+void printout_si (struct stemideal *si);
+void free_stemideal (struct stemideal *si);
+
+/*
+ * struct for working with stems in one indeterminate
+ * TODO we should eventually be able to deal with integers of any size...
+ */
+
+#define STEMSIZE(n) (sizeof(struct stem) + (n)*sizeof(Stemint))
+#define STEMIDEALSIZE(n) (sizeof(struct stemideal) + (n)*sizeof(struct stem *))
+/* provide for extra space to avoid reallocation */
+#define GB_EXTRAROOM(n) (0)
