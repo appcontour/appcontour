d9a9d0a3872fe28ba1e00e25b92eab1170355ba6
Maurizio Paolini <maurizio.paolini@unicatt.it>
2018-05-17 18:34:44 +0000

    script per testare il software per la base di Groebner

---
+++ groebner_test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# a quick way to obtain a complex set of generators that end up
+# with an ideal generate by just two polynomials
+
+#kanenobu.sh 8 -2 | ./contour --out alexander --noidealsimplify -Q --foxd 2 | ./contour alexander --experimental
+
+#
+# this should produce an integer overflow
+#
+idealkan8m2=`kanenobu.sh 10 -2 | ./contour --out alexander -Q --foxd 2`
+
+idealkan8m2bis="
+#
+# --foxd 2
+#
+# WARNING: above max allowed int size, cannot complete simplification
+# WARNING: inhibiting gcd computation due to integer size
+# *** Warning: result can be noncanonical ***
+ideal(t) {
++332177328;
++12+68240532t;
++1353876-1607472t;
++1+16057617t+55111t^2;
++69022+140468496t+544144t^2;
+-477317+19746927t-12953t^2;
++5177-82979183t+709211t^2+616t^3;
+-4513+652924t+26830052t^2+t^3;
++2589-58686413t+787863t^2+17104t^3;
+}"
+
+kanenobu.sh 10 -2 | ./contour --out alexander --foxd 2 2>/dev/null -Q
+
+ideal1="
+ideal(t) {
++9;
++3t-3;
+t^2+t+1;
+}"
+
+ideal2="
+ideal(t) {
++6;
++3t-3;
+t^2+t;
+}"
+
+ideal3="
+ideal(t) {
++9;
+3t^2+3t+3;
+}"
+
+ideal4="
+ideal(t) {
++9t^3-9;
++3t^4-3t^3-3t+3;
++t^5+t^4+t^3-t^2-t-1;
+}"
+
+ideal4bis="
+ideal(t) {
++9t^3-9;
+0;
++3t^4+6t^3-3t-6;
++t^5+t^4+t^3-t^2-t-1;
++3t^4+6t^3-3t-6;
+}"
+
+echo "$ideal4bis" | ./contour alexander --experimental
+
